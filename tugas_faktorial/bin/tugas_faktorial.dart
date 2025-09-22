9ac0a51b830d0d3b947cab17d31b191417671f48
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2025-09-22 19:49:46 +0700

    inisialisasi new project dart

---
+++ tugas_faktorial/bin/tugas_faktorial.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_faktorial/tugas_faktorial.dart' as tugas_faktorial;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_faktorial.calculate()}!');
+}
---
+++ tugas_faktorial/test/tugas_faktorial_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_faktorial/tugas_faktorial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
